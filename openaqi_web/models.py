ccc4682ee03c90ed7b8f3aaf7fff79a364eb1198
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-06 13:59:33 +0700

    Adding active sensor field

--- openaqi_web/models.py
+++ openaqi_web/models.py
@@ -9,6 +9,7 @@ class Sensor(models.Model):
     longitude = models.DecimalField(max_digits=22, decimal_places=16)
     description = models.TextField()
     secret = models.CharField(max_length=60, editable=False)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return "Sensor at " + self.name
