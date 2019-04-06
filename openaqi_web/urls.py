efef692aef603bdae0266b75cc10dddd6551c606
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-07 00:25:24 +0700

    Bootstrapping sensor logging API

--- openaqi_web/urls.py
+++ openaqi_web/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sensors/<int:id>/log', views.api_log, name='index'),
 ]
--- openaqi_web/views.py
+++ openaqi_web/views.py
@@ -1,7 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from .models import Reading, Sensor
+import json
+from json.decoder import JSONDecodeError
+
 
 def index(request):
     return HttpResponse("It works!")
+
+
+@csrf_exempt
+def api_log(request, id):
+    if request.method == "POST":
+        try:
+            sensor = Sensor.objects.get(secret=secret)
+        except Sensor.DoesNotExist:
+            return HttpResponse(status=404)
+        data = request.body.decode('utf-8')
+        return JsonResponse(request.POST, status=200)
+    else:
+        # Return a 405 (method not allowed) code
+        return HttpResponse(status=405)
