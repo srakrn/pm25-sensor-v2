bc05dea06c0c6fee0231ed7c326722935e56844a
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-06 13:01:26 +0700

    Changing admin display to sensor name

--- openaqi_web/admin.py
+++ openaqi_web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Sensor
 
 # Register your models here.
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Sensor, SensorAdmin)
