8065319d56d4dd5a09140a955ab48803247f54ab
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-07 22:09:10 +0700

    Sensor logging API

--- openaqi_web/views.py
+++ openaqi_web/views.py
@@ -15,12 +15,26 @@ def index(request):
 @csrf_exempt
 def api_log(request, id):
     if request.method == "POST":
+        # Define a set of restricted fields
+        # As `request.POST` can be accessed only once, this is needed.
+        request_body = request.POST
+        # Get a sensor with the requested ID. Return 404 if not found.
         try:
-            sensor = Sensor.objects.get(secret=secret)
+            sensor = Sensor.objects.get(id=id)
         except Sensor.DoesNotExist:
             return HttpResponse(status=404)
-        data = request.body.decode('utf-8')
-        return JsonResponse(request.POST, status=200)
+        # Check if the sensor's secret matches the requested secret
+        try:
+            if sensor.secret != request_body['secret']:
+                return HttpResponse(status=403)
+        except KeyError:
+            return HttpResponse(status=403)
+        reading_data = {f.name: request_body[f.name]
+                        for f in Reading._meta.get_fields() if f.name in request_body and f.editable}
+        logged_by = Sensor.objects.get(id=id)
+        reading = Reading(**reading_data, logged_by=logged_by)
+        reading.save()
+        return JsonResponse(reading_data, status=200)
     else:
         # Return a 405 (method not allowed) code
         return HttpResponse(status=405)
